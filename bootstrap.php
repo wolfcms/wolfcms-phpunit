a0a7b1b046d130ffae75bbcd1d70fbb74bb7d712
Martijn <martijn.niji@gmail.com>
2012-10-14 21:27:52 +0200

    Changed bootstrap file to hopefully work with travis ci.

--- bootstrap.php
+++ bootstrap.php
@@ -9,21 +9,30 @@
 
 // TODO: check include path
 ini_set('include_path', ini_get('include_path')
-                        .PATH_SEPARATOR.dirname(__FILE__).'/../../../../../opt/lampp/lib/php/PHPUnit'
                         .PATH_SEPARATOR.dirname(__FILE__).'/../src'
                         .PATH_SEPARATOR.dirname(__FILE__).'/../src/wolf'
                         .PATH_SEPARATOR.dirname(__FILE__).'/../src/wolf/app/models');
 
 //require_once 'PHPUnit/Framework.php';
 
-$_SERVER['HTTP_HOST'] = 'localhost';
+//$_SERVER['HTTP_HOST'] = 'localhost';
 
-require_once 'Framework.php';
+//require_once 'Framework.php';
 
+$dbtest = getenv('DB');
 
 // Database settings:
-define('DB_DSN', 'mysql:dbname=wolf_phpunit;host=127.0.0.1;port=3306');
-define('DB_USER', 'root');
+
+if ($dbtest == 'mysql') {
+    define('DB_DSN', 'mysql:dbname=wolfcms_test;host=127.0.0.1;port=3306');
+    define('DB_USER', 'root');
+}
+
+if ($dbtest == 'postgresql') {
+    define('DB_DSN', 'pgsql:dbname=wolfcms_test;host=127.0.0.1;port=5432');
+    define('DB_USER', 'postgres');
+}
+
 define('DB_PASS', '');
 define('TABLE_PREFIX', '');
 
